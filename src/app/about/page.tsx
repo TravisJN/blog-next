9cc5b032b2ea8142c30107aa1f9b365cccd31cff
platopixel <128953996+platopixel@users.noreply.github.com>
2025-01-10 14:52:42 -0700

    add headshot

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,12 @@
+import Image from 'next/image';
+import './about.css';
+
+import profileImage from '@/assets/profile.jpg';
+
 function About() {
     return (
         <div className="px-8 max-w-4xl text-center flex flex-col gap-4">
+            <Image src={profileImage} alt="Travis Neufeld" width={200} height={200} className="rounded-full self-center mb-4 headshot" />
             <p>My name is Travis Neufeld and I am a software engineer with about a decade of experience building apps for web, mobile, TV, and gaming consoles.</p>
             <p>I have extensive experience working with JavaScript, TypeScript, React, React Native, and Node.</p>
             <p>I also am an experienced Scrum master who enjoys seeking continuous improvement.</p>
--- src/app/portfolio/@apps/outofinsight/page.tsx
+++ src/app/portfolio/@apps/outofinsight/page.tsx
@@ -1,5 +1,12 @@
 function Outofinsight() {
-    return <div>outofinsight</div>;
+    return (
+        <div>
+            <p className="font-bold">Outofinsight</p>
+            <p>Built with React and Firebase</p>
+            <p>Outofinsight is a website where I can write down and save any ideas I have. I wanted a singular place to save my ideas that was accessible from anywhere.</p>
+            <a href="https://outofinsight.com">outofinsight.com</a>
+        </div>
+    );
 }
 
 export default Outofinsight;

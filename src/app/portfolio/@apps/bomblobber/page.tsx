0746aa153147fc934256306619149f631d5456ca
platopixel <128953996+platopixel@users.noreply.github.com>
2025-01-11 11:49:25 -0700

    add content for bomblobber

--- src/app/portfolio/@apps/bomblobber/page.tsx
+++ src/app/portfolio/@apps/bomblobber/page.tsx
@@ -1,13 +1,29 @@
+import Image from 'next/image';
+
+import githubMark from '@/assets/github-mark-white.png';
+import githubLogo from '@/assets/github-logo-white.png';
+
 function Bomblobber() {
     return (
-        <iframe
-            title="bomblobber"
-            style={{ border: 'none' }}
-            height="600px"
-            width="600px"
-            id="bomblobber-game"
-            src="../bomblobber/bomblobber.html"
-        />
+        <div className="flex flex-col gap-4">
+            <p className="font-bold">Bomblobber</p>
+            <div className="flex items-center gap-2">
+                <p>Built with vanilla JS with Canvas</p>
+                <a href="https://github.com/TravisJN/bomblobber" className="flex items-center gap-1 border rounded p-1">
+                    <Image src={githubMark} width={20} height={20} alt="github" />
+                    <Image src={githubLogo} height={20} alt="github" />
+                </a>
+            </div>
+            <p>Bomblobber is a physics simulation game I made while taking some computer science courses at Santa Rosa Junior College. This was a precursor to my more complete game, Boom Boom. Check it out by clicking to launch a missile.</p>
+            <iframe
+                title="bomblobber"
+                style={{ border: 'none' }}
+                height="600px"
+                width="600px"
+                id="bomblobber-game"
+                src="../bomblobber/bomblobber.html"
+            />
+        </div>
     );
 }
 

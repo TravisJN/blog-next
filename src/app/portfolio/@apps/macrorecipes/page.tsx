cd77f26354cd38c82be2df4bb455981a4887b3c8
TravisJN <tdizzletrav@gmail.com>
2025-01-13 11:45:43 -0700

    add content for macro recipes

--- src/app/portfolio/@apps/macrorecipes/page.tsx
+++ src/app/portfolio/@apps/macrorecipes/page.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image';
 
 import githubMark from '@/assets/github-mark-white.png';
 import githubLogo from '@/assets/github-logo-white.png';
-// import macroRecipesImage from '@/assets/macrorecipes.png';
+import macroRecipesImage1 from '@/assets/macrorecipes1.png';
+import macroRecipesImage2 from '@/assets/macrorecipes2.png';
+import macroRecipesImage3 from '@/assets/macrorecipes3.png';
 
 function Macrorecipes() {
     return (
@@ -11,17 +13,18 @@ function Macrorecipes() {
                 <p className="font-bold">Macro Recipes</p>
             </div>
             <div className="flex items-center gap-2">
-                <p>Built with React</p>
-                <a href="https://github.com/TravisJN/crapsbattle" className="flex items-center gap-1 border rounded p-1">
+                <p>Built with React Native and the Spoonacular API</p>
+                <a href="https://github.com/TravisJN/macro-recipes" className="flex items-center gap-1 border rounded p-1">
                     <Image src={githubMark} width={20} height={20} alt="github" />
                     <Image src={githubLogo} height={20} alt="github" />
                 </a>
             </div>
-            <div className="linkEffect linkEffect--insideOut mr-auto">
-                <a href="https://outofinsight.com" target="_blank" rel="noopener noreferrer">https://outofinsight.com</a>
+            <p>Macro Recipes is an iOS app built with React Native that allows users to enter their desired macronutrient amount and retrieve recipes that fit the selected criteria.</p>
+            <div className="flex">
+                <Image src={macroRecipesImage1} width={300} style={{ objectFit: 'contain' }} alt="macro recipes app 1" className="rounded" />
+                <Image src={macroRecipesImage2} width={300} style={{ objectFit: 'contain' }} alt="macro recipes app 2" className="rounded" />
+                <Image src={macroRecipesImage3} width={300} style={{ objectFit: 'contain' }} alt="macro recipes app 3" className="rounded" />
             </div>
-            <p>Outofinsight is a website where I can write down and save any ideas I have. I wanted a singular place to save my ideas that was accessible from anywhere.</p>
-            {/* <Image src={macroRecipesImage} alt="macro recipes" className="rounded" /> */}
         </div>
     );
 }

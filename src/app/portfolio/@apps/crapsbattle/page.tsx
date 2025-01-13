f2fcb61df489f3de4747c1c8a2e0967afd3a50c9
TravisJN <tdizzletrav@gmail.com>
2025-01-13 11:33:03 -0700

    add content for crapsbattle

--- src/app/portfolio/@apps/crapsbattle/page.tsx
+++ src/app/portfolio/@apps/crapsbattle/page.tsx
@@ -1,5 +1,26 @@
+import Image from 'next/image';
+
+import githubMark from '@/assets/github-mark-white.png';
+import githubLogo from '@/assets/github-logo-white.png';
+import crapsBattleImage from '@/assets/crapsbattle.png';
+
 function Crapsbattle() {
-    return <div>Crapsbattle</div>;
+    return (
+        <div className="flex flex-col gap-4 mb-24">
+            <div className="flex">
+                <p className="font-bold">Craps Battle</p>
+            </div>
+            <div className="flex items-center gap-2">
+                <p>Built with React</p>
+                <a href="https://github.com/TravisJN/crapsbattle" className="flex items-center gap-1 border rounded p-1">
+                    <Image src={githubMark} width={20} height={20} alt="github" />
+                    <Image src={githubLogo} height={20} alt="github" />
+                </a>
+            </div>
+            <p>{`Craps Battle is a dice game made with React. The goal is to defeat your opponent by rolling 6 dice three times and developing a "hand" of both attack and defense to be used each round.`}</p>
+            <Image src={crapsBattleImage} alt="macro recipes" className="rounded" />
+        </div>
+    );
 }
 
 export default Crapsbattle;
--- src/app/portfolio/@apps/macrorecipes/page.tsx
+++ src/app/portfolio/@apps/macrorecipes/page.tsx
@@ -1,5 +1,29 @@
+import Image from 'next/image';
+
+import githubMark from '@/assets/github-mark-white.png';
+import githubLogo from '@/assets/github-logo-white.png';
+// import macroRecipesImage from '@/assets/macrorecipes.png';
+
 function Macrorecipes() {
-    return <div>Macrorecipes</div>;
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex">
+                <p className="font-bold">Macro Recipes</p>
+            </div>
+            <div className="flex items-center gap-2">
+                <p>Built with React</p>
+                <a href="https://github.com/TravisJN/crapsbattle" className="flex items-center gap-1 border rounded p-1">
+                    <Image src={githubMark} width={20} height={20} alt="github" />
+                    <Image src={githubLogo} height={20} alt="github" />
+                </a>
+            </div>
+            <div className="linkEffect linkEffect--insideOut mr-auto">
+                <a href="https://outofinsight.com" target="_blank" rel="noopener noreferrer">https://outofinsight.com</a>
+            </div>
+            <p>Outofinsight is a website where I can write down and save any ideas I have. I wanted a singular place to save my ideas that was accessible from anywhere.</p>
+            {/* <Image src={macroRecipesImage} alt="macro recipes" className="rounded" /> */}
+        </div>
+    );
 }
 
 export default Macrorecipes;
